3689a25f65c2941d3ff56c6282662635cfbc80a1
Hristo Todorov <todorov.h@gmail.com>
2018-02-04 00:32:27 +0200

    generating board dom with nested loops

--- src/components/board.component.js
+++ src/components/board.component.js
@@ -2,30 +2,25 @@ import React from 'react'
 
 import Square from './square.component'
 
-function renderSquare(props, squareIndex) {
-
-  return <Square value={props.squares[squareIndex]} onClick={() => props.onClick(squareIndex)} />;
-}
-
 export default function Board(props) {
 
   return (
     <div>
-      <div className="board-row">
-        {renderSquare(props, 0)}
-        {renderSquare(props, 1)}
-        {renderSquare(props, 2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(props, 3)}
-        {renderSquare(props, 4)}
-        {renderSquare(props, 5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(props, 6)}
-        {renderSquare(props, 7)}
-        {renderSquare(props, 8)}
-      </div>
+      {Array(3).fill(null).map((_, row) => {
+        const currentRow = row * 3;
+        return (
+          <div key={row} className="board-row">
+            {Array(3).fill(null).map((_, col) => {
+              const squareIndex = currentRow + col
+              return (
+                <Square key={col}
+                  value={props.squares[squareIndex]}
+                  onClick={() => props.onClick(squareIndex)} />
+              )
+            })}
+          </div>
+        )
+      })}
     </div>
   );
 }
