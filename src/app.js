119b375b48a2f773ebff40490e306e1839852247
Hristo Todorov <todorov.h@gmail.com>
2018-02-05 00:05:23 +0200

    port

--- src/app.js
+++ src/app.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
     res.sendfile('./tic-tac-toe-tutorial/build/index.js')
 })
 
-app.listen(3000, () => console.log('demo-server listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('demo-server listening on port 3000!'))
