c99da63c4c7d902f0ec0559624675722a146b232
Hristo Todorov <todorov.h@gmail.com>
2018-02-09 21:17:23 +0200

    folder structure

--- to-do-redux/src/index.js
+++ to-do-redux/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
 import './index.css';
-import App from './App';
+
+import App from './presentation/App';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
